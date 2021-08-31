2b3de3247635bbb57928be2b3510d0af9d800456
Filip Kosmala <kosmala.filip@gmail.com>
2021-08-31 18:12:32 +0200

    Initial commit

---
+++ VrChatCompanion/VrChatCompanion/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using VrChatCompanion.Models;
+using VrChatCompanion.ViewModels;
+
+namespace VrChatCompanion.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
